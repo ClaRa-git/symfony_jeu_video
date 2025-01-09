bc121605ae611db4d88e75224fc7b7b4667ac0fa
ClaRa-git <rameau.claire.cr@gmail.com>
2025-01-09 12:10:36 +0100

    Création de la fonction loadUsers

--- www/src/DataFixtures/AppFixtures.php
+++ www/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -18,9 +19,45 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Appel de la méthode pour générer des utilisateurs
+        $this->loadUsers($manager);
 
         $manager->flush();
+    }
+
+    /**
+     * Méthode pour générer des utilisateurs
+     * @param ObjectManager $manager
+     * @return void
+     */
+    public function loadUsers(ObjectManager $manager): void
+    {
+        // Création d'un tableau avec les infos des utilisateurs
+        $array_users = [
+            [
+                'email' => "admin@admin.com",
+                'password' => 'admin',
+                'roles' => ['ROLE_ADMIN'],
+                'username' => 'administrateur'
+            ],
+            [
+                'email' => "user@user.com",
+                'password' => 'user',
+                'roles' => ['ROLE_USER'],
+                'username' => 'utilisateur'
+            ]
+        ];
+
+        // Boucle sur le tableau pour créer les utilisateurs
+        foreach ($array_users as $value) {
+            $user = new User();
+            $user->setEmail($value['email']);
+            $user->setPassword($this->encoder->hashPassword($user, $value['password']));
+            $user->setRoles($value['roles']);
+            $user->setUsername($value['username']);
+
+            // Persister les données
+            $manager->persist($user);
+        }
     }
 }
