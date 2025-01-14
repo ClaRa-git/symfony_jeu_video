18bb0949d21e62fe49c6f9764f37d10b07f3e9a7
ClaRa-git <rameau.claire.cr@gmail.com>
2025-01-14 11:58:14 +0100

    Ajout de restriction d'accès

--- www/assets/app.js
+++ www/assets/app.js
@@ -9,6 +9,7 @@ import './styles/app.scss';
 import './styles/detail.scss';
 import './styles/navbar.scss';
 import './styles/login.scss';
+import './styles/admin.scss';
 
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import './bootstrap';
